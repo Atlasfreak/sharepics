517793299a488f5fd159d5d13f4c279caae34d21
Per <atlasfreak@gmx.de>
2024-06-24 01:41:05 +0200

    update HomePage to get files and display them as TemplateTiles

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,17 +1,43 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:sharepics/src/components/template_tile.dart';
 import 'package:sharepics/src/pages/add_template_page.dart';
 import 'package:sharepics/src/globals.dart' as globals;
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   HomePage({super.key});
 
   static const routeName = "/";
 
-  final List<String> files = ["abc", "test123", "12345"];
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<File> _files = [];
+
+  Future<List<File>> _getFiles() async {
+    return Directory(await globals.generateTemplatePath())
+        .list()
+        .where((event) => event.path.endsWith(".svg"))
+        .where((event) => event is File)
+        .map((event) => event as File)
+        .toList();
+  }
+
+  @override
+  void initState() {
+    _getFiles().then(
+      (value) {
+        setState(() {
+          _files = value.toList();
+        });
+      },
+    );
+    print(_files);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,19 +49,36 @@ class HomePage extends StatelessWidget {
         ),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: GridView.builder(
-        restorationId: "homePage",
-        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 150, mainAxisExtent: 150),
-        itemCount: 30,
-        itemBuilder: (context, index) {},
-      ),
+      body: _files.isNotEmpty
+          ? GridView.builder(
+              restorationId: "homePage",
+              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                  maxCrossAxisExtent: 150, mainAxisExtent: 150),
+              itemCount: _files.length,
+              itemBuilder: (context, index) {
+                File file = _files[index];
+                String name = globals.fileNameRegex
+                    .allMatches(file.path)
+                    .toList()[0]
+                    .group(1)
+                    .toString();
+                return TemplateTile(
+                  name: name,
+                  svgPath: file.path,
+                );
+              },
+            )
+          : const Center(
+              child: Text(
+              "Keine Vorlagen gefunden",
+              style: TextStyle(fontSize: 20),
+            )),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(
               context,
               MaterialPageRoute(
-                builder: (context) => AddTemplatePage(),
+                builder: (context) => const AddTemplatePage(),
               ));
         },
         child: const Icon(Icons.add),
