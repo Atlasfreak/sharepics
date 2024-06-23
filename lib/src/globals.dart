24adbc859e1168437625145d6778c91e0e6d5da7
Per <atlasfreak@gmx.de>
2024-06-24 00:51:39 +0200

    add regex pattern for file name

--- lib/src/globals.dart
+++ lib/src/globals.dart
@@ -3,6 +3,9 @@ library sharepics.globals;
 import 'package:path_provider/path_provider.dart';
 
 const String templatesDir = "templates";
+final RegExp fileNameRegex =
+    RegExp(r"^(?:\/.*\/)(.*?)(?:\.svg)?$", unicode: true);
+
 Future<String> Function() generateTemplatePath = () async =>
     "${(await getApplicationDocumentsDirectory()).path}/$templatesDir";
 
